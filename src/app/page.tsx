964f06b0b08830c4fad792c6c0aaf66b08e35ca8
Yaroslav <shipilov@umich.edu>
2024-05-11 15:58:19 -0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,157 @@
-import Image from "next/image";
+// import {
+//   Table,
+//   TableBody,
+//   TableCaption,
+//   TableCell,
+//   TableHead,
+//   TableHeader,
+//   TableRow,
+// } from "@/components/ui/table"
+
+
+
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen flex-col max-w-4xl mx-auto p-8">
+//       <h1 className="text-3xl font-bold mb-4">Plastic Chemicals Tests</h1>
+
+//       <Table>
+//         <TableCaption>A list of your recent invoices.</TableCaption>
+//         <TableHeader>
+//           <TableRow>
+//             <TableHead className="w-[100px]">Invoice</TableHead>
+//             <TableHead>Status</TableHead>
+//             <TableHead>Method</TableHead>
+//             <TableHead className="text-right">Amount</TableHead>
+//           </TableRow>
+//         </TableHeader>
+//         <TableBody>
+//           <TableRow>
+//             <TableCell className="font-medium">INV001</TableCell>
+//             <TableCell>Paid</TableCell>
+//             <TableCell>Credit Card</TableCell>
+//             <TableCell className="text-right">$250.00</TableCell>
+//           </TableRow>
+//         </TableBody>
+//       </Table>
+
+//     </main>
+//   );
+// }
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+})
 
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex min-h-screen flex-col max-w-4xl mx-auto p-8">
+      <h1 className="text-3xl font-bold">Plastic Chemicals Tests</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="my-4 space-y-6 max-w-sm">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field: { ...field } }) => (
+              <FormItem>
+                <FormLabel>Subscribe to updates</FormLabel>
+                <div className="flex gap-4">
+                  <FormControl>
+                    <Input type="email" placeholder="name@example.com" {...field} />
+                  </FormControl>
+                  <Button type="submit">Submit</Button>
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[150px]">Category</TableHead>
+            <TableHead>Good</TableHead>
+            <TableHead className="w-[200px] text-right">Total phthalates per serving (nanograms)</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">Infant Food</TableCell>
+            <TableCell>Gerber Mealtime for Baby Harvest Turkey Dinner (glass with lined lid)</TableCell>
+            <TableCell className="text-right">4,267</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Infant Food</TableCell>
+            <TableCell>Similac Advance Infant Milk-Based Powder Formula (can)</TableCell>
+            <TableCell className="text-right">4,202</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Infant Food</TableCell>
+            <TableCell>Beech-Nut Fruities Pouch Pear, Banana & Raspberries (pouch)</TableCell>
+            <TableCell className="text-right">2,826</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Infant Food</TableCell>
+            <TableCell>Gerber Cereal for Baby Rice (plastic)</TableCell>
+            <TableCell className="text-right">1,599</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Infant Food</TableCell>
+            <TableCell>Happy Baby Organics Clearly Crafted Banana & Strawberries (glass with lined lid)</TableCell>
+            <TableCell className="text-right">1,300</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Infant Food</TableCell>
+            <TableCell>Happy Baby Organic Milk-Based Infant Powder Formula With Iron (plastic)</TableCell>
+            <TableCell className="text-right">977</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Infant Food</TableCell>
+            <TableCell>Gerber Organic for Baby Pouch Apple Zucchini Spinach Strawberry (pouch)</TableCell>
+            <TableCell className="text-right">706</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </main>
   );
 }
