0389ad476118c4675f45e9939ecf54e258ee66c1
Yaroslav <shipilov@umich.edu>
2024-05-11 16:16:51 -0700

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,21 @@ const fontSans = FontSans({
 
 export const metadata: Metadata = {
   title: "Plastic Chemicals Tests",
-  description: "Finding out how much plastic US consumers eat every day.",
+  description: "We are finding out how much plastic US consumers eat every day.",
+  metadataBase: new URL('https://plasticlist.vercel.app/'),
+  openGraph: {
+    title: "Plastic Chemicals Tests",
+    description: "We are finding out how much plastic US consumers eat every day.",
+    url: "https://plasticlist.vercel.app/",
+    images: [
+      {
+        url: "/og-image.svg",
+        width: 1200,
+        height: 630,
+        alt: "Plastic Chemicals Tests",
+      },
+    ],
+  },
 };
 
 type RootLayoutProps = {
