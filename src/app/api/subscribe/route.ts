cdd01b61de50e3df9163809da48fe6020995ec9f
Deet <48701612+Deetschoe@users.noreply.github.com>
2024-05-12 18:43:20 -0400

    removed yarn and stopped logging api key

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -2,13 +2,9 @@ import { NextResponse } from 'next/server';
 import Airtable from 'airtable';
 
 export async function POST(request: Request) {
-  // Log the environment variables to ensure they are set correctly
-  console.log("Airtable API Key:", process.env.AIRTABLE_PAT_HEREE);
-  console.log("Airtable Base ID:", process.env.AIRTABLE_BASE_ID);
-
   try {
     const { email } = await request.json();
-    const base = new Airtable({ apiKey: process.env.AIRTABLE_PAT_HEREE }).base(process.env.AIRTABLE_BASE_ID as string);
+    const base = new Airtable({ apiKey: process.env.AIRTABLE_PAT }).base(process.env.AIRTABLE_BASE_ID as string);
     const existingRecords = await base('Emails').select({ filterByFormula: `email = '${email}'` }).firstPage();
     
     if (existingRecords.length === 0) {
@@ -17,7 +13,7 @@ export async function POST(request: Request) {
     
     return NextResponse.json({ message: 'Email added successfully' });
   } catch (error) {
-    console.error('Error adding email to Airtable:', error);
-    return NextResponse.json({ error: 'Failed to add email' }, { status: 500 }); // Return JSON error response
+    console.error('Error occurred when adding email. Please check server logs for more details.');
+    return NextResponse.json({ error: 'Failed to add email' }, { status: 500 });
   }
 }
