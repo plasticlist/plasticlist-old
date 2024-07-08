a6fcac56f1bed141b8a16955eff1b9c9cdb6aace
Yaroslav <shipilov@umich.edu>
2024-07-08 12:11:52 -0700

    add alpha note to the list

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <header className="flex items-center justify-between border-b pb-2">
       <a href="/" className='text-3xl font-bold'>plasticlist</a>
       <div className="flex items-center gap-8">
-        <a href="/list">the list</a>
+        <a href="/list">the list (alpha)</a>
         <a href="https://discord.gg/ZcH6ywXcTR" target="_blank" rel="noopener noreferrer">
           <Image src="/icons/discord.svg" alt="Discord Logo" width={30} height={30} />
         </a>
